8ea2c1cb95e31e461e64c57f92fcf04d2fdf89d5
Leonidas <leonidascobain@gmail.com>
2021-05-26 09:50:37 -0400

    Gerar build com modelo tradicional - WAR

--- src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
+++ src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
@@ -5,19 +5,32 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest // Classe de test para testar um repositorio
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) // Não substituir as configurações do banco de dados
+@ActiveProfiles("test") // Force esse profile como ativo no momento
 public class CursoRepositoryTest {
 
     @Autowired
     private CursoRepository repository;
+
+    @Autowired
+    TestEntityManager entityManager;
     
     @Test
     public void deveriaCarregarUmCursoAoBuscarPeloNome(){
         String nomeCurso = "HTML 5";
+
+        Curso html5 = new Curso();
+        html5.setNome(nomeCurso);
+        html5.setCategoria("Programação");
+        entityManager.persist(html5);
 
         Curso curso = this.repository.findByNome(nomeCurso);
 
